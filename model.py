26bc7cdf8e7ed27b1722b5e059aa4750546dfe76
Alex Jeleniewski <alexjan100@hotmail.com>
2023-03-22 15:33:34 +0100

    make code cleaner

--- model.py
+++ model.py
@@ -2,8 +2,8 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 
-from config import DECAY, LEARNING_RATE, SLIDING_WINDOW_STEP, SLIDING_WINDOW_LENGTH, NUM_CLASSES, LSTM_HIDDEN_CHANNELS, \
-    NUM_SENSOR_CHANNELS, CONV_HIDDEN_CHANNELS, DROP_RATE, EPOCHS
+from config import DECAY, LEARNING_RATE, NUM_CLASSES, LSTM_HIDDEN_CHANNELS, \
+    NUM_SENSOR_CHANNELS, CONV_HIDDEN_CHANNELS, DROP_RATE, EPOCHS, FILTER_SIZE, BATCH_SIZE
 from opportunity_dataset import OpportunityDataset
 
 
@@ -137,10 +137,10 @@ def test(test_loader, net, criterion):
 class DeepConvLSTM(nn.Module):
     def __init__(self):
         super(DeepConvLSTM, self).__init__()
-        self.cl2 = nn.Conv2d(1, CONV_HIDDEN_CHANNELS, (5, 1), groups=1)
-        self.cl3 = nn.Conv2d(CONV_HIDDEN_CHANNELS, CONV_HIDDEN_CHANNELS, (5, 1), groups=1)
-        self.cl4 = nn.Conv2d(CONV_HIDDEN_CHANNELS, CONV_HIDDEN_CHANNELS, (5, 1), groups=1)
-        self.cl5 = nn.Conv2d(CONV_HIDDEN_CHANNELS, CONV_HIDDEN_CHANNELS, (5, 1), groups=1)
+        self.cl2 = nn.Conv2d(1, CONV_HIDDEN_CHANNELS, (FILTER_SIZE, 1))
+        self.cl3 = nn.Conv2d(CONV_HIDDEN_CHANNELS, CONV_HIDDEN_CHANNELS, (FILTER_SIZE, 1))
+        self.cl4 = nn.Conv2d(CONV_HIDDEN_CHANNELS, CONV_HIDDEN_CHANNELS, (FILTER_SIZE, 1))
+        self.cl5 = nn.Conv2d(CONV_HIDDEN_CHANNELS, CONV_HIDDEN_CHANNELS, (FILTER_SIZE, 1))
         self.flatten = nn.Flatten()
         self.dropout = nn.Dropout(DROP_RATE)
         self.rec6 = nn.LSTM(CONV_HIDDEN_CHANNELS * NUM_SENSOR_CHANNELS, LSTM_HIDDEN_CHANNELS, batch_first=True)
@@ -173,6 +173,7 @@ class DeepConvLSTM(nn.Module):
         x = self.fc8(x)
 
         x = self.softmax(x)
+
         return x
 
 
@@ -182,24 +183,26 @@ def init_params(params_iter):
 
 
 if __name__ == "__main__":
-    print("loading data")
+    print("Loading and applying sliding window over data")
     training_dataloader = DataLoader(
         OpportunityDataset("data/pre-processed.pkl", train_or_test="train"),
-        batch_size=100)
+        batch_size=BATCH_SIZE)
     test_dataloader = DataLoader(
         OpportunityDataset("data/pre-processed.pkl", train_or_test="test"),
-        batch_size=100)
-    print("data loaded")
+        batch_size=BATCH_SIZE)
+    print("Data loaded and sliding window applied")
 
     net = DeepConvLSTM()
 
     loss_criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.RMSprop(net.parameters(), lr=LEARNING_RATE, weight_decay=DECAY)
+
     init_params(net.parameters())
+
     train(training_dataloader, test_dataloader, net, optimizer, loss_criterion)
 
-    print("saving model")
+    print("Saving model")
     torch.save(net.state_dict(), "DeepConvLSTM_Opportunity_Model.pt")
-    print("model saved")
-    print("testing model")
+    print("Model saved")
+    print("Testing model")
     test(test_dataloader, net, loss_criterion)
--- opportunity_dataset.py
+++ opportunity_dataset.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 
 from torch.utils.data.dataset import T_co
 
-from config import SLIDING_WINDOW_LENGTH, SLIDING_WINDOW_STEP
+from config import SLIDING_WINDOW_LENGTH, SLIDING_WINDOW_STEP, NUM_SENSOR_CHANNELS, NUM_CLASSES
 
 
 class OpportunityDataset(Dataset):
@@ -17,37 +17,46 @@ class OpportunityDataset(Dataset):
             (train_data, train_labels), (test_data, test_labels) = data
         if train_or_test == "train":
             self.sensor_data = train_data.astype("float32")
-            self.labels = train_labels.astype("int32")
-
-            (self.sensor_data, self.labels) = self.open_sliding_window_over_data()
+            self.labels = train_labels.astype("int64")
         elif train_or_test == "test":
             self.sensor_data = test_data.astype("float32")
-            self.labels = test_labels.astype("int32")
-
-            (self.sensor_data, self.labels) = self.open_sliding_window_over_data()
+            self.labels = test_labels.astype("int64")
         else:
             raise ValueError("Invalid argument, use train or test")
+        (self.sensor_data, self.labels) = self.open_sliding_window_over_data()
 
     def open_sliding_window_over_data(self, window_size=SLIDING_WINDOW_LENGTH, step_size=SLIDING_WINDOW_STEP):
-        print('opening sliding window of data')
         flatted_sensor_data = self.sensor_data.ravel()
         flatted_labels = self.labels.ravel()
 
-        data_x = sliding_window_view(flatted_sensor_data, window_size*113)
-        data_x = data_x[::step_size*113]
+        data_x = sliding_window_view(flatted_sensor_data, window_size*NUM_SENSOR_CHANNELS)
+        data_x = data_x[::step_size*NUM_SENSOR_CHANNELS]
+        data_x = self.__truncate_to_length_of_labels(data_x, flatted_labels)
+        data_x = np.reshape(data_x, (-1, window_size, NUM_SENSOR_CHANNELS))
+
         data_y = sliding_window_view(flatted_labels, window_size)[::step_size]
-        data_x = data_x[:len(data_y)]
-
-        data_x = np.reshape(data_x, (-1, window_size, 113))
         data_y = np.reshape(data_y, (-1, window_size))
-
-        data_y = np.asarray([i[-1] for i in data_y])
-        n_values = 18
-        data_y = np.eye(n_values)[data_y]
+        data_y = self.__convert_windows_to_single_values(data_y)
+        data_y = self.__to_one_hot_encoded(data_y)
 
         data_y = data_y.astype("int64")
 
         return data_x, data_y
+
+    @staticmethod
+    def __truncate_to_length_of_labels(data, labels):
+        return data[:len(labels)]
+
+    def __convert_windows_to_single_values(self, windows):
+        return np.asarray([self.__get_value_of_last_element_in_window(window) for window in windows])
+
+    @staticmethod
+    def __get_value_of_last_element_in_window(window):
+        return window[-1]
+
+    @staticmethod
+    def __to_one_hot_encoded(labels):
+        return np.eye(NUM_CLASSES)[labels]
 
     def __len__(self):
         return len(self.sensor_data)
